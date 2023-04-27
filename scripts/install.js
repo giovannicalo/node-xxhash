507bc177535ad604eb9bd19806e47bf400b97cde
Giovanni Calò <giovanni.calo@live.it>
2023-04-28 00:05:47 +0100

    Fixed an issue with the install script producing corrupted files

--- scripts/install.js
+++ scripts/install.js
@@ -1,19 +1,19 @@
-const { promises: { mkdir, rename } } = require("fs");
-const { get } = require("https");
+const { createWriteStream, promises: { mkdir, rename, unlink } } = require("fs");
 const { join } = require("path");
+const { Readable, promises: { finished } } = require("stream");
 
-const { Extract } = require("unzipper");
+const extractZip = require("extract-zip");
 
-get(
-	"https://codeload.github.com/Cyan4973/xxHash/zip/refs/tags/v0.8.1",
-	async (response) => {
-		const path = join(__dirname, "..", "libraries");
-		await mkdir(path, { recursive: true });
-		response.pipe(Extract({ path })).on("close", async () => {
-			await rename(
-				join(path, "xxHash-0.8.1"),
-				join(path, "xxhash")
-			);
-		});
-	}
-);
+(async () => {
+	const path = join(__dirname, "..", "libraries");
+	await mkdir(path, { recursive: true });
+	const response = await fetch("https://codeload.github.com/Cyan4973/xxHash/zip/refs/tags/v0.8.1");
+	const filePath = join(path, "xxhash.zip");
+	await finished(Readable.fromWeb(response.body).pipe(createWriteStream(filePath)));
+	await extractZip(filePath, { dir: path });
+	rename(
+		join(path, "xxHash-0.8.1"),
+		join(path, "xxhash")
+	);
+	unlink(filePath);
+})();
